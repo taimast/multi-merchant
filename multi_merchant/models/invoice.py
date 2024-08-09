ff5698e8516ac9e0fd0666b2f39e8d756a28fddf
taimast <charlskenno@gmail.com>
2024-08-09 22:09:13 +0300

    update expire_at

--- multi_merchant/models/invoice.py
+++ multi_merchant/models/invoice.py
@@ -49,7 +49,7 @@ class Invoice(Base):
     amount: Mapped[float | None]
     invoice_id: Mapped[str] = mapped_column(String(50), index=True)
     expire_at: Mapped[datetime.datetime | None] = mapped_column(
-        default_factory=lambda: func.now() + datetime.timedelta(seconds=PAYMENT_LIFETIME)
+        default=lambda: func.now() + datetime.timedelta(seconds=PAYMENT_LIFETIME)
     )
     extra_data: Mapped[dict] = mapped_column(JSON, default={})
     pay_url: Mapped[str | None] = mapped_column(String(255))
